c8e81f987e9f0daa53f5aff71a54f444c623f739
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-28 09:00:32 -0400

    Refactor OAuthButtons component to support dynamic background color and icon for each OAuth provider

--- components/OAuthButtons.tsx
+++ components/OAuthButtons.tsx
@@ -11,6 +11,7 @@ type OAuthProvider = {
   name: Provider;
   displayName: string;
   icon?: string;
+  color: string;
 };
 
 const OAuthButtons = () => {
@@ -19,21 +20,25 @@ const OAuthButtons = () => {
       id: 1,
       name: "github",
       displayName: "GitHub",
-      icon: "/github.png",
+      icon: "/github.svg",
+      color: "#000000", // black
     },
     {
       id: 2,
       name: "google",
       displayName: "Google",
       icon: "/google.png",
+      color: "#fff", // white
     },
     {
       id: 3,
       name: "discord",
       displayName: "Discord",
       icon: "/discord.png",
+      color: "#7289DA", // blue
     },
   ];
+
   return (
     <>
       {oAuthProviders.map((provider) => (
@@ -41,13 +46,14 @@ const OAuthButtons = () => {
           variant={"default"}
           onClick={async () => await oAuthSignIn(provider.name)}
           key={provider.id}
-          className="mt-2 flex items-center justify-center gap-2"
+          className={`mt-2 flex items-center justify-center gap-2 ${provider.name === "github" ? "text-white" : "text-black"}`}
+          style={{ backgroundColor: provider.color }} // Inline style for dynamic background color
         >
           <Image
             src={provider?.icon as string}
             width={23}
             height={23}
-            alt={`${provider.displayName} image`}
+            alt={`${provider.displayName} icon`}
           />
           {provider.displayName}
         </Button>
