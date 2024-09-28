2d74b44eac56f3c78ab3c2a41644d50d0e1ce457
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-28 09:45:11 -0400

    Refactor dashboard components and constants

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,3 +1,8 @@
+import DailyGoal from "@/components/dashboard/daily_goal";
+import WeeklyOverview from "@/components/dashboard/weekly_overview";
+import RecentTopics from "@/components/dashboard/recent_topics";
+import ProductivityStats from "@/components/dashboard/productivity_stats";
+
 export const weekDays: { id: number; name: string }[] = [
   { id: 1, name: "Mon" },
   { id: 2, name: "Tue" },
@@ -6,4 +11,11 @@ export const weekDays: { id: number; name: string }[] = [
   { id: 5, name: "Fri" },
   { id: 6, name: "Sat" },
   { id: 7, name: "Sun" },
+];
+
+export const DashboardConent = [
+  { id: 1, component: DailyGoal },
+  { id: 2, component: WeeklyOverview },
+  { id: 3, component: RecentTopics },
+  { id: 4, component: ProductivityStats },
 ];
