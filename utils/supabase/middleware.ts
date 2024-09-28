f98925ba38060c7ef58e4e3d13f5cc0143ed78d1
Sajjadalgburi <dfgsajjad@gmail.com>
2024-09-28 09:45:04 -0400

    Refactor supabase middleware to handle session updates and redirect to home page after successful sign-in

--- utils/supabase/middleware.ts
+++ utils/supabase/middleware.ts
@@ -22,34 +22,30 @@ export const updateSession = async (request: NextRequest) => {
           },
           setAll(cookiesToSet) {
             cookiesToSet.forEach(({ name, value }) =>
-              request.cookies.set(name, value),
+              request.cookies.set(name, value)
             );
             response = NextResponse.next({
               request,
             });
             cookiesToSet.forEach(({ name, value, options }) =>
-              response.cookies.set(name, value, options),
+              response.cookies.set(name, value, options)
             );
           },
         },
-      },
+      }
     );
 
     // This will refresh session if expired - required for Server Components
     // https://supabase.com/docs/guides/auth/server-side/nextjs
     const user = await supabase.auth.getUser();
 
-    // protected routes
-    if (request.nextUrl.pathname.startsWith("/protected") && user.error) {
-      return NextResponse.redirect(new URL("/sign-in", request.url));
-    }
-
-    if (request.nextUrl.pathname === "/" && !user.error) {
-      return NextResponse.redirect(new URL("/protected", request.url));
+    if (request.nextUrl.pathname === "/sign-in" && !user.error) {
+      return NextResponse.redirect(new URL("/", request.url));
     }
 
     return response;
   } catch (e) {
+    console.error(e);
     // If you are here, a Supabase client could not be created!
     // This is likely because you have not set up environment variables.
     // Check out http://localhost:3000 for Next Steps.
